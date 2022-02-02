f5e981476ebc3368f9802cea2f0540af797edc2b
javed <engineer-code@wadic.net>
2022-02-03 03:17:15 +0500

    add gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,6 @@ module ApplicationHelper
     gravatar_id = Digest::MD5::hexdigest(chef.email.downcase)
     size = options[:size]
     gravatar_url = "https://www.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: chef.email, class: "gravatar")
+    image_tag(gravatar_url, alt: chef.chefname, class: "gravatar")
   end
 end
